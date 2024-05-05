530bb0c563d04f6736c1dc63e38337de4c61b44f
EdgarKlavins <klavins.e@gmail.com>
2024-05-05 16:51:17 +0000

    added several products and products views

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 from .models import Product, Genre, Author, Year
 
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+        'year',
+        'genre',
+        'price',
+        'rating',
+        'image_url',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Year)
