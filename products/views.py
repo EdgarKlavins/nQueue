6542124d6614169bec70bce350dbfaa1e36ddec9
EdgarKlavins <klavins.e@gmail.com>
2024-05-06 16:14:41 +0000

    added query to genres

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Genre
 
 # Create your views here.
 
@@ -7,9 +9,36 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+    genre = None
+
+    if request.GET:
+
+        if 'genre' in request.GET:
+            genre = request.GET.get('genre').split(',')
+            products = products.filter(genre__name__in=genre)
+            genre = Genre.objects.filter(name__in=genre)
+
+
+
+
+        
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter search criteria!")
+                return redirect(reverse('products'))
+            
+            queries =Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+            
+            if not products.exists():
+                messages.warning(request, "No products found matching the search criteria.")
 
     context = {
         'products': products,
+        'search_term' : query,
+        'current_genre' : genre,
     }
 
     return render(request, 'products/products.html', context)
