a9d2c392bcb29175335428afc8adaf0268738708
EdgarKlavins <klavins.e@gmail.com>
2024-05-06 17:21:23 +0000

    adding sorting options for users

--- products/views.py
+++ products/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Genre
-
-# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -11,41 +10,56 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     genre = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            elif sortkey == 'year':
+                sortkey = '-year' if 'direction' in request.GET and request.GET['direction'] == 'desc' else 'year'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+
+            products = products.order_by(sortkey)
 
         if 'genre' in request.GET:
             genre = request.GET.get('genre').split(',')
             products = products.filter(genre__name__in=genre)
             genre = Genre.objects.filter(name__in=genre)
 
-
-
-
-        
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "Please enter search criteria!")
                 return redirect(reverse('products'))
-            
-            queries =Q(name__icontains=query) | Q(description__icontains=query)
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-            
+
             if not products.exists():
                 messages.warning(request, "No products found matching the search criteria.")
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
-        'search_term' : query,
-        'current_genre' : genre,
+        'search_term': query,
+        'current_genre': genre,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
 
-
 def product_description(request, product_id):
-    """ A view to show seperate product description  """
+    """ A view to show separate product description  """
 
     product = get_object_or_404(Product, pk=product_id)
 
@@ -54,3 +68,4 @@ def product_description(request, product_id):
     }
 
     return render(request, 'products/product_description.html', context)
+
