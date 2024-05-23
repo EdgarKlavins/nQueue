364aacb26d1ec0cb7db06c5796724357adf1b0f6
EdgarKlavins <klavins.e@gmail.com>
2024-05-23 18:33:38 +0000

    linked up stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -3,12 +3,15 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from bag.contextx import bag_contents
+from bag.contexts import bag_contents
 
 import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -17,12 +20,21 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request,'Stripe public key is missing')
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51PJceLDlpOACMTtH7AJC8NnoMmBjTpfI7hYGKydx1Ae2TbMOfk8Fu08T7wdW6bVsHPFs8TthTk6sLTBjYhNwuLHo00X5tByMRM',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
         
     }
 
